984f7412ce05ecbe0f1cd4ef5a6f0756c95a1f56
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2019-09-07 19:31:06 -0400

    Added sphere map to cube map conversion

--- src/main.cpp
+++ src/main.cpp
@@ -18,18 +18,33 @@
 void initQuad(IndexedModel&);
 void initSkyboxCube(IndexedModel&);
 
+void sphereToCube(RenderContext& context, Texture& sphereMap,
+		uint32 texLength, const std::string& textureFolder);
+void cubeToSphere(RenderContext& context, CubeMap& cubeMap,
+		uint32 texWidth, uint32 texHeight, const std::string& fileName);
+
 int main() {
 	HDRBitmap bmp;
-	bmp.load("./res/Alexs_Apt_2k.hdr");
+	//bmp.load("./res/Alexs_Apt_2k.hdr");
+	bmp.load("./test.hdr");
+
+	//Bitmap pngBMP;
+	
+	//bmp.save("./test.hdr");
 
 	float aspectRatio = (float)bmp.getWidth() / (float)bmp.getHeight();
 
 	//Display display("Cube Map to Sphere Map", 800, (int32)(800.f / aspectRatio));
-	Display display("Cube Map to Sphere Map", 800, 600);
+	Display display("Cube Map to Sphere Map", 800, 800);
 
 	RenderContext context;
 
 	Texture texture(context, bmp, GL_RGB32F);
+
+	//pngBMP.load(texture);
+	//pngBMP.save("./res/test-tex.png");
+
+	//sphereToCube(context, texture, 512, "./res/test-skybox");
 
 	std::string cubeTextures[] = {"./res/skybox/right.jpg",
 		"./res/skybox/left.jpg", "./res/skybox/top.jpg",
@@ -63,18 +78,43 @@ int main() {
 
 	RenderTarget screen(context, display.getWidth(), display.getHeight());
 
-	Camera camera(glm::radians(70.f), aspectRatio, 0.1f, 100.f);
+	Camera camera(glm::radians(90.f), 1.f, 0.1f, 10.f);
 
 	//quadShader.setVector2f("screenSize", glm::vec2(display.getWidth(), display.getHeight()));
+
+	//camera.rotate(glm::radians(0.f), glm::radians(0.f));
+	
+	glm::mat4 persp = glm::perspective(glm::radians(90.f), 1.f, 0.1f, 10.f);
+
+	glm::mat4 captureViews[] = {
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f, -1.0f,  0.0f), glm::vec3(0.0f,  0.0f, -1.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f,  0.0f,  1.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
+	};
+
+	int32 counter = 0;
+	int32 index = 0;
+
+	glViewport(0, 0, display.getWidth(), display.getHeight());
 
 	while (!display.isCloseRequested()) {
 		screen.clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		camera.rotate(0.f, 0.005f);
+		//camera.rotate(0.f, 0.005f);
+		//camera.update();
 
-		camera.update();
+		++counter;
 
-		skyboxCube.updateBuffer(1, glm::value_ptr(camera.getViewProjection()), sizeof(glm::mat4));
+		if (counter > 100) {
+			counter = 0;
+			index = (index + 1) % 6;
+		}
+
+		skyboxCube.updateBuffer(1, glm::value_ptr(persp * captureViews[index]),
+				sizeof(glm::mat4));
 
 		//skyboxShader.setSampler("skybox", skybox, sampler, 0);
 		//context.draw(screen, skyboxShader, skyboxCube, GL_TRIANGLES);
@@ -140,4 +180,51 @@ void initSkyboxCube(IndexedModel& model) {
 
 	model.addIndices3i(1, 5, 7);
 	model.addIndices3i(7, 3, 1);
+}
+
+void sphereToCube(RenderContext& context, Texture& sphereMap,
+		uint32 texLength, const std::string& textureFolder) {
+	Texture buffer(context, texLength, texLength, GL_RGBA);
+	RenderTarget target(context, buffer, GL_COLOR_ATTACHMENT0);
+
+	Sampler sampler(context, GL_LINEAR, GL_LINEAR);
+
+	std::stringstream ss;
+	Util::resolveFileLinking(ss, "./src/sphere-skybox.glsl", "#include");
+	Shader shader(context, ss.str());
+
+	IndexedModel cubeModel;
+	initSkyboxCube(cubeModel);
+
+	VertexArray cube(context, cubeModel, GL_STATIC_DRAW);
+
+	Bitmap bmp;
+
+	glm::mat4 persp = glm::perspective(glm::radians(90.f), 1.f, 0.1f, 10.f);
+
+	glm::mat4 captureViews[] = {
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f, -1.0f,  0.0f), glm::vec3(0.0f,  0.0f, -1.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f,  0.0f,  1.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
+		glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
+	};
+
+	std::string sides[] = {"right", "left", "top", "bottom", "front", "back"};
+
+	shader.setSampler("skybox", sphereMap, sampler, 0);
+
+	glViewport(0, 0, texLength, texLength);
+
+	for (uint32 i = 0; i < 6; ++i) {
+		target.clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		cube.updateBuffer(1, glm::value_ptr(persp * captureViews[i]), sizeof(glm::mat4));
+
+		context.draw(target, shader, cube, GL_TRIANGLES);
+
+		bmp.load(buffer);
+		bmp.save(textureFolder + "/" + sides[i] + ".png");
+	}
 }
